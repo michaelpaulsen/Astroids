c0644a9fddcb86eda3e409614536bfb0f505327e
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-07-06 18:12:35 -0700

    Create Player.h
    made player class

--- Astroids.cpp
+++ Astroids.cpp
@@ -4,9 +4,13 @@
 #include <winuser.h>
 #include "Window.cpp"
 #include "Game.cpp"
+#include "Player.h"
+#include "../Skele_lib/utils/Utils.h"
 int main()
 {
-	/**windows vars */
+	
+	Player player1 = Player(Screen::width/2,Screen::height/2);
+
 	SetConsoleActiveScreenBuffer(Window::hConsole);
 	bool playing = true;
 	/**game loop*/
---
+++ Player.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include "Screen.h"
+class Player {
+	int x, y, r;
+public:
+	Player() {
+		this->x = 0;
+		this->y = 0;
+		this->r = 0;
+	}
+	Player(int x, int y) {
+		this->x = x;
+		this->y = y;
+		this->r = 0;
+	}
+	int getX() {
+		return this->x;
+	}
+	int getY() {
+		return this->y;
+	}
+	int getR() {
+		return this->r;
+	}
+	void setX(int n) {
+		this->x = n;
+	}
+	void setY(int n) {
+		this->y = n;
+	}
+	void setR(int n) {
+		this->r = n;
+	}
+	char getDisplayCharFromAngle() {
+		if (this->r < 90) {
+			return '╩';
+		}
+		else if (this->r < 180) {
+			return '╠';
+		}
+		else if (this->r < 270) {
+			return '╦';
+		}
+		else {
+			return '╣';
+		}
+	}
+};
